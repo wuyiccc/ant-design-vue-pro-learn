dd7eadbf003a03d0f3d03fafb2fcd271397d9386
wuyiccc <3406324191@qq.com>
2020-12-26 22:38:38 +0800

    导入ant-design-vue的less样式表, 并解决导入less之后项目报错问题

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Antd from 'ant-design-vue'
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.less";
 
 Vue.use(Antd);
 Vue.config.productionTip = false;
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+/**
+ * @author wuyiccc
+ * @date 2020/12/26 22:33
+ * 岂曰无衣, 与子同袍
+ */
+// vue.config.js
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  }
+};
