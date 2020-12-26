57b4eb23ec468ef3e488df21e5676990c5122bc5
wuyiccc <3406324191@qq.com>
2020-12-26 22:57:51 +0800

    1. 利用babel-plugin-import实现按需加载, 需要重新npm run serve 2. 需要安装对应的插件: npm i --save-dev babel-plugin-import

--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Button from "ant-design-vue/lib/button";
-// import "ant-design-vue/dist/antd.less";
-import "ant-design-vue/lib/button/style";
+import { Button } from "ant-design-vue";
+
+Vue.config.productionTip = false;
 
 Vue.use(Button);
-Vue.config.productionTip = false;
 
 new Vue({
   router,
