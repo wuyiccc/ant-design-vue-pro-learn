518eb09196b5899285167c7b67bf4c48c12898e4
wuyiccc <3406324191@qq.com>
2020-12-26 22:43:58 +0800

    将全局导入修改为按需导入

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Antd from "ant-design-vue";
-import "ant-design-vue/dist/antd.less";
+import Button from "ant-design-vue/lib/button";
+// import "ant-design-vue/dist/antd.less";
+import "ant-design-vue/lib/button/style";
 
-Vue.use(Antd);
+Vue.use(Button);
 Vue.config.productionTip = false;
 
 new Vue({
