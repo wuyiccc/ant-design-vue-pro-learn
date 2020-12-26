d18e0de33c0f9ced4d5a638b4eb0cea03b69f2a2
wuyiccc <3406324191@qq.com>
2020-12-26 22:06:06 +0800

    导入ant-design-vue

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Antd from 'ant-design-vue'
 
+Vue.use(Antd);
 Vue.config.productionTip = false;
 
 new Vue({
