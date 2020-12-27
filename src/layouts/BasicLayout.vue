075cf78bf08e00577b07c8146fef1b371fa33e16
wuyiccc <3406324191@qq.com>
2020-12-27 13:55:46 +0800

    1. 利用ant design vue 的layout组件库设置核心路由布局

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,10 +1,25 @@
 <!--核心页面布局-->
 <template>
   <div>
-    <Header></Header>
-    <SiderMenu></SiderMenu>
-    <router-view></router-view>
-    <Footer></Footer>
+    <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+      <!--菜单栏-->
+      <a-layout-sider v-model="collapsed" collapsible>
+        <div class="logo" />
+        <SiderMenu></SiderMenu>
+      </a-layout-sider>
+      <!--核心布局-->
+      <a-layout>
+        <a-layout-header style="background: #fff; padding: 0">
+          <Header></Header>
+        </a-layout-header>
+        <a-layout-content style="margin: 0 16px">
+          <router-view></router-view>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+          <Footer></Footer>
+        </a-layout-footer>
+      </a-layout>
+    </a-layout>
   </div>
 </template>
 
@@ -15,6 +30,11 @@ import SiderMenu from "./SiderMenu";
 
 export default {
   name: "BasicLayout",
+  data() {
+    return {
+      collapsed: false
+    };
+  },
   components: {
     Header,
     Footer,
