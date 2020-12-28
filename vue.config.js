aeecfd1d09488afc45575fe8406bd04ad84ccf19
wuyiccc <3406324191@qq.com>
2020-12-29 06:36:56 +0800

    修改主题色

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,9 @@ module.exports = {
   css: {
     loaderOptions: {
       less: {
+        modifyVars: {
+          "primary-color": "#1DA57A"
+        },
         javascriptEnabled: true
       }
     }
