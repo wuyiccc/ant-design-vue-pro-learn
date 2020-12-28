e8d969f189dab4a09519fb33bb4ac19bd3eea048
wuyiccc <3406324191@qq.com>
2020-12-29 00:40:38 +0800

    表单自动校验功能

--- src/views/Forms/BasicForm.vue
+++ src/views/Forms/BasicForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-form :layout="formLayout">
+  <a-form :layout="formLayout" :form="form">
     <a-form-item
       label="Form Layout"
       :label-col="formItemLayout.labelCol"
@@ -24,17 +24,24 @@
       label="Field A"
       :label-col="formItemLayout.labelCol"
       :wrapper-col="formItemLayout.wrapperCol"
-      :validateStatus="fieldAStatus"
-      :help="fieldAHelp"
     >
-      <a-input v-model="fieldA" placeholder="input placeholder" />
+      <a-input
+        v-decorator="[
+          'fieldA',
+          {
+            initialValue: fieldA,
+            rules: [{ required: true, min: 6, message: '必须大于5个字符' }]
+          }
+        ]"
+        placeholder="input placeholder"
+      />
     </a-form-item>
     <a-form-item
       label="Field B"
       :label-col="formItemLayout.labelCol"
       :wrapper-col="formItemLayout.wrapperCol"
     >
-      <a-input v-model="fieldB" placeholder="input placeholder" />
+      <a-input v-decorator="['fieldB']" placeholder="input placeholder" />
     </a-form-item>
     <a-form-item :wrapper-col="buttonItemLayout.wrapperCol">
       <a-button type="primary" @click="handleSubmit">
@@ -47,24 +54,12 @@
 <script>
 export default {
   data() {
+    this.form = this.$form.createForm(this);
     return {
       formLayout: "horizontal",
-      fieldA: "",
-      fieldAStatus: "",
-      fieldAHelp: "",
+      fieldA: "hello",
       fieldB: ""
     };
-  },
-  watch: {
-    fieldA() {
-      if (this.fieldA.length <= 5) {
-        this.fieldAStatus = "error";
-        this.fieldAHelp = "必须大于5个字符";
-      } else {
-        this.fieldAStatus = "";
-        this.fieldAHelp = "";
-      }
-    }
   },
   computed: {
     formItemLayout() {
@@ -90,14 +85,11 @@ export default {
       this.formLayout = e.target.value;
     },
     handleSubmit() {
-      if (this.fieldA.length <= 5) {
-        this.fieldAStatus = "error";
-        this.fieldAHelp = "必须大于5个字符";
-      } else {
-        console.log(this.fieldA, this.fieldB);
-        this.fieldAStatus = "";
-        this.fieldAHelp = "";
-      }
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log(values);
+        }
+      });
     }
   }
 };
