fe27a2c6092e3693ec9b6527aa52da1c79df15d1
wuyiccc <3406324191@qq.com>
2020-12-29 06:21:12 +0800

    使用assets文件夹中的图片

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,12 +1,20 @@
 <template>
   <div style="text-align: center">
     <icon-font type="icon-icon-404" style="font-size: 100px"></icon-font>
+    <img :src="logo" alt="">
   </div>
 </template>
 
 <script>
+import logo from "@/assets/logo.png";
+
 export default {
-  name: "404"
+  name: "404",
+  data() {
+    return {
+      logo
+    };
+  }
 };
 </script>
 
